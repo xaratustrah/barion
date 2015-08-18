c88e35670fc436b38c1daebbd59e3dee136db75f
xaratustrah <shahab@zamail.de>
2015-08-18 18:20:18 +0200

    messed with setup.py

--- setup_win.py
+++ setup_win.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-#!/usr/bin/env python
+# !/usr/bin/env python
 """
 Barion
 
@@ -9,36 +8,40 @@ Jul 2015 Xaratustrah
 
 """
 
-import py2exe
+from distutils.core import setup
 
-VERSION='0.0.1'
+name = 'barion'
 
-PKG = ['fortranformat']
+version = '0.0.1'
+
+pkgs = ['fortranformat']
 
 includes = ['sip',
-			'PyQt5',
-			'PyQt5.QtWidgets',
-			'PyQt5.QtCore',
-			'PyQt5.QtGui']
+            'PyQt5',
+            'PyQt5.QtWidgets',
+            'PyQt5.QtCore',
+            'PyQt5.QtGui']
+
+options = {'bundle_files': 1,
+           'compressed': True,
+           'includes': includes,
+           'packages': pkgs
+           }
 
 datafiles = [("platforms", ["C:\\Python34\\Lib\\site-packages\\PyQt5\\plugins\\platforms\\qwindows.dll"]),
-			("", [r"c:\windows\syswow64\MSVCP100.dll", r"c:\windows\syswow64\MSVCR100.dll"])]
+             ("", [r"c:\windows\syswow64\MSVCP100.dll", r"c:\windows\syswow64\MSVCR100.dll"])]
 
 setup(
-	name='barion',
-    version=VERSION,
-	url='',
-	license='',
-	zipfile=None,
-	data_files=datafiles,
-	windows=[{
-			'script': 'barion.py',
-			'icon_resources': [(1, 'icon.ico')],
-			'dest_base':'barion'
-			}], 
-	options={'py2exe':{
-		'bundle_files': 1,
-		'compressed': True,
-		'includes': includes, 
-		'packages': PKG
-		}})
+    name=name,
+    version=version,
+    url='',
+    license='',
+    zipfile=None,
+    data_files=datafiles,
+    windows=[{
+        'script': 'barion.py',
+        'icon_resources': [(1, 'icon.ico')],
+        'dest_base': 'barion'
+    }],
+    options={'py2exe': options}
+)
