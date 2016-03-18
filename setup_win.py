712a99318d46b082c751e1a3a77cd033ae27b9a6
Xaratustrah <shahab.sanjari@gmail.com>
2016-03-18 11:49:25 +0100

    change setup_win.py

--- setup_win.py
+++ setup_win.py
@@ -14,17 +14,30 @@ and @carlkl
 
 from version import __version__
 from distutils.core import setup
-import os, sys
-import py2exe
 from glob import glob
 import PyQt5
+import py2exe
 
-NAME="barion"
+NAME = 'barion'
+
+packages = ['fortranformat']
+
+includes = ['sip', 'atexit', ]
+
+excludes = None
+
+options = {
+    'optimize': 2,
+    'compressed': True,
+    'includes': includes,
+    'excludes': excludes,
+    'packages': packages
+}
+
 data_files = []
-qt_platform_plugins = [("platforms", glob(PyQt5.__path__[0] + r'\plugins\platforms\*.*'))]
-data_files.extend(qt_platform_plugins)
 
-sys.argv.append('py2exe')
+qt_platform_plugins = [('platforms', glob(PyQt5.__path__[0] + r'\plugins\platforms\*.*'))]
+data_files.extend(qt_platform_plugins)
 
 setup(
     name=NAME,
@@ -33,18 +46,9 @@ setup(
     license='GPLv.3',
     zipfile=None,
     data_files=data_files,
-    windows=[
-        {
-            "script": "barion.py",
-             'icon_resources': [(1, 'rsrc/icon.ico')]
-        }
-    ],
-    options={
-        "py2exe": {
-            "includes":["sip", "atexit",],
-            "compressed": True,
-            "optimize": 2,
-            'packages': ['fortranformat']
-        }
-    }
+    windows=[{
+        'script': 'barion.py',
+        'icon_resources': [(1, 'rsrc/icon.ico')]
+    }],
+    options={'py2exe': options}
 )
